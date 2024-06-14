ebc61744c9b0240d1d3e70f18313d93edf46409b
iminurcomputerkid <167721852+iminurcomputerkid@users.noreply.github.com>
2024-06-13 23:52:38 -0400

    Create Main.java

---
+++ Dictionary Class/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing Dictionary Methods");
+        DictionaryDriver.testOne();
+        DictionaryDriver.testTwo();
+        DictionaryDriver.testThree();
+        DictionaryDriver.testFour();  
+    }
+}
